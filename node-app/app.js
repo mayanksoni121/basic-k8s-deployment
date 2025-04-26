748fdc70c634da2db3d229508bbe662d6f42936b
Mayank Soni <mayank@midships.io>
2025-04-26 10:40:40 +0530

    Configmap and secret

--- node-app/app.js
+++ node-app/app.js
@@ -1,9 +1,30 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const port = 3000
+const message1 = process.env.MESSAGE1 || "";
+const message2 = process.env.MESSAGE2 || "";
+const username = process.env.USERNAME || "";
+const password = process.env.PASSWORD || "";
 
 app.get('/', (req, res) => {
-  res.send('Hello Developers!')
+  res.send(`<h1>Hello Developers! </h1>`)
+})
+
+app.get('/message1', (req, res) => {
+  res.send(`<h1>Hello Developers! ${message1}</h1>`)
+})
+
+app.get('/message2', (req, res) => {
+  res.send(`<h1>Hello Developers! ${message2}</h1>`)
+})
+
+app.get('/secrets', (req, res) => {
+  res.send(`
+    <h1>Username: ${username} </h1>
+    <h1>Password: ${password} </h1>
+    `)
 })
 
 app.listen(port, () => {
